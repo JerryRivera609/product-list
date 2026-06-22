51607124b8377ad14629010d527e71ff766967b3
JerryRivera609 <jerry609rivera@gmail.com>
2026-06-22 14:48:07 -0500

    change

--- app/page.tsx
+++ app/page.tsx
@@ -219,7 +219,7 @@ export default function Home() {
           </div>
 
           <nav className="flex-1 px-3 py-5 flex items-start justify-center">
-            <button className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-2 py-2 text-left text-sm font-semibold text-black">
+            <button className="flex w-full items-center justify-center   gap-3 rounded-md bg-white px-2 py-2 text-left text-sm font-semibold text-black">
               <Package size={19} />
               {!isSidebarCollapsed && <span>Productos</span>}
             </button>
